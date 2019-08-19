381b8d5a82706c1a5311624f9d8a364e9b04af82
hobbes543 <39176746+hobbes543@users.noreply.github.com>
2019-08-19 19:52:30 -0400

    File reading

--- Decompile/src/main.cpp
+++ Decompile/src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
-	cout << "Hello World";
+	ifstream rom;
+	char rom_buffer[0x1000];
+
+	rom.open("Roms/Stars_Sergey_Naydenov_2010.ch8", ios::in | ios::binary | ios::ate);
+	
+	if (rom.is_open())
+	{
+		cout << "File is open";
+		rom.seekg(0, ios::beg);
+		rom.read(rom_buffer, 0x1000);
+		rom.close();
+	}
+	else {
+		cout << "File is not open";
+		ofstream log;
+		log.open("log.txt");
+		log << "ch8 file did not load.";
+		log.close();
+	}
+	
+	return 0;
 }
